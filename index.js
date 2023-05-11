cf72f8e88d25ffeb6853e6ea363c32cbc8de49ea
NijatS <nijatjs@code.edu.az>
2023-05-12 01:14:50 +0400

    solve color problem

--- index.js
+++ index.js
@@ -5,10 +5,59 @@ const purpleColor = document.querySelector("#purple");
 const blueColor = document.querySelector("#blue");
 const yellowColor = document.querySelector("#yellow");
 const greenColor = document.querySelector("#green");
-const colors = document.querySelector("#colors");
 const saveButton = document.querySelector("#save");
-colors.forEach((color) => {
-  color.addEventListener("click", () => {
-    color.focus();
-  });
+
+const cards = document.querySelector("#cards");
+const body = document.body;
+
+let color = [redColor, purpleColor, blueColor, yellowColor, greenColor];
+let selectedColor;
+redColor.addEventListener("click", () => {
+  Blur(color);
+  Click(redColor);
+  selectedColor = redColor;
+  body.style.backgroundColor = String(selectedColor.style.backgroundColor);
 });
+purpleColor.addEventListener("click", () => {
+  Blur(color);
+  Click(purpleColor);
+  selectedColor = purpleColor.style.backgroundColor;
+});
+blueColor.addEventListener("click", () => {
+  Blur(color);
+  Click(blueColor);
+});
+yellowColor.addEventListener("click", () => {
+  Blur(color);
+  Click(yellowColor);
+});
+greenColor.addEventListener("click", () => {
+  Blur(color);
+  Click(greenColor);
+});
+saveButton.addEventListener("click", () => {
+  let title = titleInput.value.trim();
+  let text = noteInput.value.trim();
+  const newCard = document.createElement("div");
+  newCard.innerHTML = `
+  <div>
+            <h4>${title}</h4>
+            <img src="./Image/trash-icon.svg" alt="Trash Icon" />
+          </div>
+          <p>
+           ${text}
+          </p>
+  `;
+  newCard.style.borderColor = selectedColor;
+  newCard.className = "card";
+  cards.append(newCard);
+});
+
+function Click(body) {
+  body.textContent = "✓";
+}
+function Blur(color) {
+  for (const c of color) {
+    c.textContent = "";
+  }
+}
