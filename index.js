a81cb51d3e8098add31c82409260a7e2ec2eeb98
NijatS <nijatjs@code.edu.az>
2023-05-12 17:04:03 +0400

    fix:search input

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const titleInput = document.querySelector("#titleInput");
 const noteInput = document.querySelector("#noteInput");
+const searchInput = document.querySelector("#searchInput");
+
 const redColor = document.querySelector("#red");
 const purpleColor = document.querySelector("#purple");
 const blueColor = document.querySelector("#blue");
 const yellowColor = document.querySelector("#yellow");
 const greenColor = document.querySelector("#green");
+
 const saveButton = document.querySelector("#save");
 
+const cards = document.querySelector("#cards");
+const card = document.getElementsByClassName("card");
 const defaultCard = document.querySelector("#default");
 const defaultTrash = defaultCard.querySelector("img");
 
@@ -14,6 +19,7 @@ let style;
 let selectedColor;
 
 let color = [redColor, purpleColor, blueColor, yellowColor, greenColor];
+let cardsArray = [defaultCard];
 
 redColor.addEventListener("click", () => {
   Blur(color);
@@ -73,18 +79,30 @@ saveButton.addEventListener("click", () => {
   cardDiv.style.backgroundColor.opacity = "0.4";
   newCard.className = "card";
   cards.append(newCard);
+  cardsArray.push(newCard);
   Blur(color);
   titleInput.value = "";
   noteInput.value = "";
   selectedColor = "";
   const trash = newCard.querySelector("img");
   trash.addEventListener("click", () => {
+    let index = cardsArray.findIndex((card) => card == newCard);
     newCard.remove();
+    cardsArray.splice(index, 1);
   });
+});
+searchInput.addEventListener("keyup", () => {
+  const matchedCard = Search();
+  if (matchedCard.length != 0) {
+    for (const card of matchedCard) {
+      card.style.display = "none";
+    }
+  }
 });
 
 defaultTrash.addEventListener("click", () => {
   defaultCard.remove();
+  cardsArray.shift();
 });
 function Click(body) {
   body.textContent = "✓";
@@ -93,4 +111,20 @@ function Blur(color) {
   for (const c of color) {
     c.textContent = "";
   }
+}
+function Search() {
+  let matchedCard = [];
+  for (const card of cardsArray) {
+    card.style.display = "initial";
+    const text = card.children[1].textContent.trim().toLowerCase();
+    const title = card.children[0].children[0].textContent.trim().toLowerCase();
+    if (
+      text.startsWith(searchInput.value.trim()) ||
+      title.startsWith(searchInput.value.trim())
+    ) {
+    } else {
+      matchedCard.push(card);
+    }
+  }
+  return matchedCard;
 }
