72a75d65da0a55eaa0a64e70d592e113ecfec9f6
NijatS <nijatjs@code.edu.az>
2023-10-05 08:53:39 +0400

    Updated Site Logo

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const blueColor = document.querySelector("#blue");
 const yellowColor = document.querySelector("#yellow");
 const greenColor = document.querySelector("#green");
 
+const addButton = document.querySelector("#add");
 const saveButton = document.querySelector("#save");
 
 const cards = document.querySelector("#cards");
 const card = document.getElementsByClassName("card");
 const defaultCard = document.querySelector("#default");
-const defaultTrash = defaultCard.querySelector("img");
+const defaultTrash = defaultCard.querySelector("#trash");
+const defaultEdit = defaultCard.querySelector("#edit");
 
 let style;
 let selectedColor;
@@ -32,7 +34,7 @@ for (const c of color) {
   });
 }
 
-saveButton.addEventListener("click", () => {
+addButton.addEventListener("click", () => {
   if (!titleInput.value.trim() || !noteInput.value.trim()) {
     alert("Please write note title");
     return;
@@ -45,13 +47,12 @@ saveButton.addEventListener("click", () => {
   let text = noteInput.value.trim();
   const newCard = document.createElement("div");
   newCard.innerHTML = `
-  <div>
+        <div>
             <h4>${title}</h4>
-            <img src="./Image/trash-icon.svg" alt="Trash Icon" />
-          </div>
-          <p>
-           ${text}
-          </p>
+            <img src="./Image/edit-icon.svg" alt="Edit Icon" id="edit">
+            <img src="./Image/trash-icon.svg" alt="Trash Icon" id="trash" />
+        </div>
+          <p>${text}</p>
   `;
   const cardDiv = newCard.getElementsByTagName("div")[0];
   newCard.style.borderColor = selectedColor;
@@ -63,7 +64,7 @@ saveButton.addEventListener("click", () => {
   titleInput.value = "";
   noteInput.value = "";
   selectedColor = "";
-  const trash = newCard.querySelector("img");
+  const trash = newCard.querySelector("#trash");
   trash.addEventListener("click", () => {
     if (!confirm("Are you sure?")) {
       return;
@@ -72,6 +73,39 @@ saveButton.addEventListener("click", () => {
     newCard.remove();
     cardsArray.splice(index, 1);
   });
+  const edit = newCard.querySelector("#edit");
+  edit.addEventListener("click", () => {
+    // if (!confirm("Are you sure?")) {
+    //   return;
+    // }
+    titleInput.value = newCard.children[0].children[0].textContent;
+    noteInput.value = newCard.children[1].textContent;
+    for (const c of color) {
+      let cardColor = getComputedStyle(c);
+      if (cardColor.backgroundColor == newCard.style.borderColor) {
+        Blur(color);
+        Click(c);
+        selectedColor = cardColor.backgroundColor;
+        divColor = selectedColor.substring(0, selectedColor.length - 1);
+        divColor += ",0.4)";
+      }
+    }
+    saveButton.addEventListener("click", () => {
+      for (const card of cardsArray) {
+        if (newCard == card) {
+          card.children[0].children[0].textContent = titleInput.value;
+          card.children[1].textContent = noteInput.value;
+          cardsArray[index] = card;
+          const cardDiv = card.getElementsByTagName("div")[0];
+          card.style.borderColor = selectedColor;
+          cardDiv.style.backgroundColor = divColor;
+        }
+      }
+    });
+  });
+  titleInput.value = "";
+  noteInput.value = "";
+  selectedColor = "";
 });
 searchInput.addEventListener("keyup", () => {
   const matchedCard = Search();
@@ -88,6 +122,20 @@ defaultTrash.addEventListener("click", () => {
   }
   defaultCard.remove();
   cardsArray.shift();
+});
+defaultEdit.addEventListener("click", () => {
+  // if (!confirm("Are you sure?")) {
+  //   return;
+  // }
+  titleInput.value = defaultCard.children[0].children[0].textContent;
+  noteInput.value = defaultCard.children[1].textContent;
+  for (const c of color) {
+    let cardColor = getComputedStyle(c);
+    if (cardColor.backgroundColor == defaultCard.style.borderColor) {
+      Blur(color);
+      Click(c);
+    }
+  }
 });
 function Click(body) {
   body.textContent = "✓";
